2d63ba10e3e2a19a67776aa5eeb7e2b896689f56
M-Kwizera <m.kwizera@alustudent.com>
2023-05-20 06:19:49 -0700

    edited the output

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/env ruby
-#0-simply_match_school.rb
+# 0-simply_match_school.rb
 argument1 = ARGV[0] # First argument passed to the script
+match_data = argument1.scan(/\bSchool\b/)
 
-if arg.match(/\bSchool\b/)
-    puts "The argument matches the pattern 'School'"
+if !match_data.empty?
+    concatenated_match_data = match_data.join
+    puts "#{concatenated_match_data}"
+
 else 
-    puts "The argument does not match the pattern 'School'"
+    puts ""
 end
