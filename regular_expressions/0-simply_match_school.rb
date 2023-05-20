4969788017c58cdd4bafd97e08e4e17f5ec8eebd
M-Kwizera <m.kwizera@alustudent.com>
2023-05-20 06:14:46 -0700

    F0-simply_match_school.rb

---
+++ regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+#0-simply_match_school.rb
+argument1 = ARGV[0] # First argument passed to the script
+
+if arg.match(/\bSchool\b/)
+    puts "The argument matches the pattern 'School'"
+else 
+    puts "The argument does not match the pattern 'School'"
+end
---
+++ regular_expressions/README.md
@@ -0,0 +1,2 @@
+# alu-scripting
+This directory (alu-scripting) holds projects on Regular expressions used in Ruby and many more scripting projects.
