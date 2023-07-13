199f73bf9cb5673ee4ee38ae6cbd732e99fbb093
Maxime <m.kwizera@alustudent.com>
2023-07-13 12:26:59 +0200

    firsst commit

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {"User-Agent": "Custom User Agent"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+
+    else:
+        return 0
+
+
+call = number_of_subscribers("https://www.reddit.com/r/Home/")
+print(call)
